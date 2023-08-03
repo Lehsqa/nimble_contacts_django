240a70edb5aab38fc1b7bf7d30b655e3c7f9b21d
devRSV <devRSV@d.c>
2023-08-03 22:26:27 +0300

    Add DRF Test

--- nimble_contacts/contacts/tests.py
+++ nimble_contacts/contacts/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import pytest
+from rest_framework.test import APIClient
+from contacts.models import Contact
 
-# Create your tests here.
+
+client = APIClient()
+
+
+@pytest.mark.django_db
+def test_get_data():
+    response = client.get('/api/v1/contacts-search?search=Oleg')
+    assert response.status_code == 201
+    contact = Contact.objects.first().value()
+    assert contact != response
